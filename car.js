e29f9017a61197bccc611489dd17fa9be718ac5f
fish <semi_beers_0h@icloud.com>
2024-11-18 16:07:32 -0600

    Add files via upload

---
+++ car.js
@@ -0,0 +1,31 @@
+class Car {
+    constructor(tempC, tempXpos, tempYpos, tempXspeed, tempXdegree) {
+        this.c = tempC;
+        this.xpos = tempXpos;
+        this.ypos = tempYpos;
+        this.xspeed = tempXspeed;
+        this.xdegree = tempXdegree;
+    }
+
+    display() {
+        push();
+        stroke(0);
+        fill(this.c);
+        translate(this.xpos, this.ypos);
+        rotate(radians(this.xdegree));
+        rectMode(CENTER);
+        rect(0,0,20,10);
+        pop();
+    }
+
+    drive() {
+        this.xpos = this.xpos + this.xspeed;
+        if (this.xpos > width) {
+            this.xpos = 0;
+        }
+    }
+  
+  flip() {
+    this.xdegree ++;
+  }
+}
